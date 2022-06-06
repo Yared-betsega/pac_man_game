cb0e4d396d0ecbc3eb7d6bf303a52a3955fbfca6
Bereket_Tilahun <btilahun287@gmail.com>
2022-06-06 14:35:18 +0300

    adding functionality and modifying  pacman file

--- pacman.py
+++ pacman.py
@@ -19,3 +19,18 @@ def init():
     glClearColor(0.0, 0.0, 0.0, 1.0)
     gluOrtho2D(-5.0, 28.0, -4.0, 20.0)
     return window
+
+
+init()
+
+font = pygame.font.Font('freesansbold.ttf', 25)
+
+def main():
+    graphicsHandler = GraphicsHandler(font, grid)
+    gameController = GameController(grid)
+    hero = Hero()
+    blackGhost = Ghost(grid, (1, len(grid[0]) - 2), 0.0, 0.0, 0.0)
+    greenGhost = Ghost(grid, (1, 1), 0.0, 1.0, 0.0)
+    whiteGhost = Ghost(grid, (len(grid)-2, len(grid[0])-2), 1.0, 1.0, 1.0)
+    tillGhost = Ghost(grid, (len(grid)-2, 1), 0.0, 1.0, 1.0)
+    nowRun, lost, won = 0, False, 0
