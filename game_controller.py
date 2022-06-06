3ec24ca56b06b2d00e25b0e581dd4d0a9a24f4bc
Yared-betsega <yaredtsegaye120@gmail.com>
2022-06-06 10:50:39 +0300

    edited the game controller

--- game_controller.py
+++ game_controller.py
@@ -12,7 +12,35 @@ class GameController:
                 if grid[i][j] == 0:
                     self.targetScore += 1
 
-    def nextGhostMove(self, current):
+    def nextGhfrom random import choice
+from game_engine import GameEngine
+from grids import *
+
+class GameController:
+    def __init__(self, grid) -> None:
+        self.grid = grid
+        self.directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        self.isValidMove = lambda point: 0 <= point[0] < len(grid)-1 and 0 <= point[1] < len(grid[0])-1 and grid[point[0]][point[1]] == 0
+        self.targetScore = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 0:
+                    self.targetScore += 1
+
+    def nextGhostMove(self, current, destination):
+        ai = GameEngine(self.grid)
+        return ai.bfs(current, destination)
+    
+    def checkLoss(self, ghost, player):
+        if ghost == player:
+            return True
+        return False
+
+    def checkWin(self, score):
+        if score == self.targetScore:
+            return True
+        return False     
+ostMove(self, current):
         while True:
             direction = choice(self.directions)
             next = (current[0] + direction[0], current[1] + direction[1])
