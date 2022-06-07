de748fa68364a527fde0d18956b9ff54cdbb99b8
Bereket_Tilahun <btilahun287@gmail.com>
2022-06-07 12:37:57 +0300

    main function

--- pacman.py
+++ pacman.py
@@ -9,9 +9,6 @@ from graphics import GraphicsHandler
 from grids import *
 from hero import Hero
 
-
-
-
 global grid
 grid = choice(grids)
 def init():
@@ -37,3 +34,33 @@ def main():
     whiteGhost = Ghost(grid, (len(grid)-2, len(grid[0])-2), 1.0, 1.0, 1.0)
     tillGhost = Ghost(grid, (len(grid)-2, 1), 0.0, 1.0, 1.0)
     nowRun, lost, won = 0, False, 0
+    
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_LEFT]:
+                if hero.x > 1 and grid[hero.x-1][hero.y] != 1:
+                    hero.moveLeft()
+                    
+            if keys[pygame.K_RIGHT]:
+                if hero.x < 29 and grid[hero.x+1][hero.y] != 1:
+                    hero.moveRight()
+                    
+            if keys[pygame.K_UP]:
+                if hero.y < 16 and grid[hero.x][hero.y+1] != 1:
+                    hero.moveUp()
+                    
+            if keys[pygame.K_DOWN]:
+                if hero.y > 1 and grid[hero.x][hero.y-1] != 1:
+                    hero.moveDown()
+
+        if not won and not lost:
+            graphicsHandler.playGround(hero)
+            if nowRun % 200 == 0:
+                blackGhostNextMove = gameController.nextGhostMove(blackGhost.currentPosition)
+                greenGhostNextMove = gameController. nextGhostMove(greenGhost.currentPosition)
+                whiteGhostNextMove = gameController.nextGhostMove(whiteGhost.currentPosition)
